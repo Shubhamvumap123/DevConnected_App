4f57604c33b6da0b810021daf08d13fbf2cab896
Shubham Umap <shubhamvumap@gmail.com>
2025-08-01 10:15:58 +0530

    ch

--- Backned/api/index.js
+++ Backned/api/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./src/routes/auth');
-const userRoutes = require('./src/routes/user');
-const projectRoutes = require('./src/routes/project');
+const authRoutes = require('../src/routes/auth');
+const userRoutes = require('../src/routes/user');
+const projectRoutes = require('../src/routes/project');
 
 dotenv.config();
 const app = express();
@@ -19,5 +19,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
